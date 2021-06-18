ecf919a31aaa54e2d8bf302c0b2a82029a889a0b
Andrew Talle <talle.andrew@gmail.com>
2021-06-18 12:30:32 -0600

    add protection to course routes

--- controllers/courses.js
+++ controllers/courses.js
@@ -3,10 +3,10 @@ const asyncHandler = require("../middleware/async");
 const Course = require("../models/Course");
 const Bootcamp = require("../models/Bootcamp");
 
-// @desc Get courses
-// @route GET /api/v1/courses
-// @route GET /api/v1/bootcamps/:bootcampId/courses
-// @access Public
+// @desc      Get courses
+// @route     GET /api/v1/courses
+// @route     GET /api/v1/bootcamps/:bootcampId/courses
+// @access    Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
   if (req.params.bootcampId) {
     const courses = await Course.find({ bootcamp: req.params.bootcampId });
@@ -21,9 +21,9 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
   }
 });
 
-// @desc Get single course
-// @route GET /api/v1/courses/:id
-// @access Public
+// @desc      Get single course
+// @route     GET /api/v1/courses/:id
+// @access    Public
 exports.getCourse = asyncHandler(async (req, res, next) => {
   const course = await Course.findById(req.params.id).populate({
     path: "bootcamp",
@@ -31,7 +31,9 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
   });
 
   if (!course) {
-    return new ErrorResponse(`No course with the id of ${req.params.id}`, 404);
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+    );
   }
 
   res.status(200).json({
@@ -40,12 +42,13 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
   });
 });
 
-// @desc Add course
-// @route GET /api/v1/bootcamps/:bootcampId/courses
-// @access Private
+// @desc      Add course
+// @route     POST /api/v1/bootcamps/:bootcampId/courses
+// @access    Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
-  console.log("add course endpoint", req.body);
+  req.body.user = req.user.id;
+
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
   if (!bootcamp) {
@@ -57,17 +60,27 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     );
   }
 
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`,
+        401
+      )
+    );
+  }
+
   const course = await Course.create(req.body);
 
-  res.status(201).json({
+  res.status(200).json({
     success: true,
     data: course,
   });
 });
 
-// @desc Update course
-// @route PUT /api/v1/courses/:id
-// @access Private
+// @desc      Update course
+// @route     PUT /api/v1/courses/:id
+// @access    Private
 exports.updateCourse = asyncHandler(async (req, res, next) => {
   let course = await Course.findById(req.params.id);
 
@@ -77,10 +90,22 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
     );
   }
 
+  // Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update course ${course._id}`,
+        401
+      )
+    );
+  }
+
   course = await Course.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
   });
+
+  course.save();
 
   res.status(200).json({
     success: true,
@@ -88,15 +113,25 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
   });
 });
 
-// @desc Delete
-// @route DELETE /api/v1/courses/:id
-// @access Private
+// @desc      Delete course
+// @route     DELETE /api/v1/courses/:id
+// @access    Private
 exports.deleteCourse = asyncHandler(async (req, res, next) => {
-  let course = await Course.findById(req.params.id);
+  const course = await Course.findById(req.params.id);
 
   if (!course) {
     return next(
       new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to delete course ${course._id}`,
+        401
+      )
     );
   }
 
--- routes/courses.js
+++ routes/courses.js
@@ -8,16 +8,19 @@ const {
 } = require("../controllers/courses");
 
 const Course = require("../models/Course");
-const advancedResults = require("../middleware/advancedResults");
 
 const router = express.Router({ mergeParams: true });
 
+const advancedResults = require("../middleware/advancedResults");
 const { protect, authorize } = require("../middleware/auth");
 
 router
   .route("/")
   .get(
-    advancedResults(Course, { path: "bootcamp", select: "name description" }),
+    advancedResults(Course, {
+      path: "bootcamp",
+      select: "name description",
+    }),
     getCourses
   )
   .post(protect, authorize("publisher", "admin"), addCourse);
