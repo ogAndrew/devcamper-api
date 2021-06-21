b31e0eb6257b6ebe19a2310c2289c97c67317e60
Andrew Talle <talle.andrew@gmail.com>
2021-06-21 16:05:59 -0600

    install npm security packages

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
-const logger = require("./middleware/loggerl");
 const morgan = require("morgan");
 const colors = require("colors");
 const fileupload = require("express-fileupload");
-const cookierParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
@@ -27,8 +32,8 @@ const app = express();
 // Body parser
 app.use(express.json());
 
-// Cookier parser
-app.use(cookierParser());
+// Cookie parser
+app.use(cookieParser());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === "development") {
@@ -37,6 +42,28 @@ if (process.env.NODE_ENV === "development") {
 
 // File uploading
 app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
@@ -63,5 +90,5 @@ const server = app.listen(
 process.on("unhandledRejection", (err, promise) => {
   console.log(`Error: ${err.message}`.red);
   // Close server & exit process
-  server.close(() => process.exit(1));
+  // server.close(() => process.exit(1));
 });
